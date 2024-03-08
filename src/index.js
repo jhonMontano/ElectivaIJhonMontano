7f8ebfc2207cf333f685cb924efcccb0167a1bfb
Alexander <jhon.montano0327@gmail.com>
2024-03-07 22:35:15 -0500

    ADD The improvements in the Gifs components are finished and styles are given with bootstrap

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ import { Gifs } from "./components/Gifs";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Gifs/>
+    <Gifs />
   </React.StrictMode>
 );
